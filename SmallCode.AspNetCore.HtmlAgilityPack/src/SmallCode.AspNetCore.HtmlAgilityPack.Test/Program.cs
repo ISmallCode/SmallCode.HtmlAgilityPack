b1bbeb25ed03769f9441ea242e137469c8d21a3e
applenele <1941973283@qq.com>
2016-06-14 19:35:10 +0800

    first

---
+++ SmallCode.AspNetCore.HtmlAgilityPack/src/SmallCode.AspNetCore.HtmlAgilityPack.Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCode.AspNetCore.HtmlAgilityPack.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HtmlDocument document = new HtmlDocument();
+            string str = @"<div><span>ddd</span></div>";
+            document.LoadHtml(str);
+             
+           var list = document.DocumentNode.SelectNodes("div");
+
+            foreach (var item in list)
+            {
+                var _list = item.SelectNodes("span");
+                foreach (var _item in _list)
+                {
+                    Console.WriteLine(_item.InnerText);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
